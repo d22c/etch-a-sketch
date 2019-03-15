834b9c20a5a57b397b36f6bc83ac7f006f7b2db9
DC <campbell.1810@osu.edu>
2019-03-14 21:58:50 -0400

    Changed max dimensions limit to 150x150

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 //TODO: Get 'hover off' effect to be delayed
 
 //Initialize prompt messages
-const promptMsg = 'Please enter an integer less than or equal to 200. This integer that will be the dimensions of your new grid. ' +
+const promptMsg = 'Please enter an integer less than or equal to 150. This integer that will be the dimensions of your new grid. ' +
     '(For example, entering 16 will create a 16x16 grid.)';
-const errorPrompt = 'Invalid input. Please enter an integer number less than 250.';
+const errorPrompt = 'Invalid input. Please enter an integer number less than 150.';
 //Initialize initial dimensions
 let dimensions = 16;
 //Get references to grid container, button container, and grid height
@@ -39,27 +39,27 @@ function createGrid(dimensionsInput) {
         //allows grid to display background color without text
         unit.appendChild(document.createElement('p'));
     }
-    console.log('creategrid');
     setRowHeight();
     enableSketch();
 }
 
-/* Calculate and set row height using gridContainer height divided by (# of columns - 1)*/
+/* Calculate and set row height using gridContainer height divided by # of columns */
 function setRowHeight() {
     let heightElems = document.querySelectorAll('div.unit p');
-    Array.from(heightElems).forEach((h) => heightElems.style = `height: calc(${gridHeight}/(${dimensions}-1))`);
+    Array.from(heightElems).forEach((h) => heightElems.style = `height: calc(${gridHeight}/${dimensions})`);
 }
 
 /* Allow grid units to be drawn on by adding and removing a CSS class */
 function enableSketch() {
-    console.log('enable sketch');
+
     let units = document.getElementsByClassName('unit');
     Array.from(units).forEach((unit) => {
+        //Addition of 'active' class when user hovers div to change color of div
         unit.addEventListener('mouseenter', function () {
             unit.classList.add('active');
 
         });
-        //Delayed removal of CSS class for a trailing effect
+        //Delayed removal of 'active' class when mouse pointer leaves div for a trailing effect
         unit.addEventListener('mouseleave', function () {
             setTimeout(() => unit.classList.remove('active'), 4000);
 
@@ -71,11 +71,11 @@ btnContainer.addEventListener('click', function () {
     let rem = dimensions%1;
     //checks if input is a valid input (integer <= 200)
     do {
-        if((rem===0)&&(dimensions<=200)) { dimensions = prompt(promptMsg); }
+        if((rem===0)&&(dimensions<=150)) { dimensions = prompt(promptMsg); }
         else { dimensions = prompt(errorPrompt); }
         rem = dimensions % 1;
 
-    } while ((rem % 1 != 0) || (dimensions > 200))
+    } while ((rem % 1 != 0) || (dimensions > 150))
 
     //add check to see if input is a number and keep prompting until it is
     createGrid(dimensions * dimensions);
